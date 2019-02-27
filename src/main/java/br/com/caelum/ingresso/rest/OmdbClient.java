b522a3cc80b403358786b4d816bdef739f08c55d
Fabricio <fanpir13@hotmail.com>
2019-02-27 20:44:42 -0300

    implementando compra de filme

--- src/main/java/br/com/caelum/ingresso/rest/OmdbClient.java
+++ src/main/java/br/com/caelum/ingresso/rest/OmdbClient.java
@@ -12,7 +12,7 @@ import br.com.caelum.ingresso.model.Filme;
 @Component
 public class OmdbClient {
 	
-	public Optional<DetalhesDoFilme> request(Filme filme){
+	public <T>Optional<T> request(Filme filme, Class<T>tClass){
 		RestTemplate client = new RestTemplate();
 		
 		String titulo = filme.getNome().replace(" ", "+");
@@ -20,8 +20,7 @@ public class OmdbClient {
 		String url = String.format("https://omdb-fj22.herokuapp.com/movie?title=%s", titulo);
 		
 		try {
-			DetalhesDoFilme detalhesDoFilme = client.getForObject(url, DetalhesDoFilme.class);
-			return Optional.ofNullable(detalhesDoFilme);
+			return Optional.of(client.getForObject(url, tClass));
 		}catch(RestClientException e) {
 			return Optional.empty();
 		}
